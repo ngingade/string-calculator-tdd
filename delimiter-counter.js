8a3d0f84ab4aa9c2038ed7664d9d060246843b63
ngingade <naveenbirur@gmail.com>
2024-09-03 20:36:39 +0530

    feat: Enhance string calculator to handle comma-delimited input and add Jest tests for empty string, single number, and multiple numbers

--- delimiter-counter.js
+++ delimiter-counter.js
@@ -1,5 +1,9 @@
-const add = (a, b) => {
-  return a + b;
+const add = (str) => {
+  if (!str || str.length === 0) {
+    return 0;
+  }
+  const allNumbers = str.split(",");
+  return allNumbers.reduce((a, b) => Number(a) + Number(b), 0);
 };
 
 module.exports = add;
--- delimiter-counter.test.js
+++ delimiter-counter.test.js
@@ -1,5 +1,13 @@
 const add = require("./delimiter-counter.js");
 
-test("add 2, 2 equal to 4", () => {
-  expect(add(2, 2)).toBe(4);
+test("Empty string", () => {
+  expect(add()).toBe(0);
+});
+
+test("Pass string '1', add the numbers and sum should be equal to 1", () => {
+  expect(add("1")).toBe(1);
+});
+
+test("Pass string '2,2', add the numbers and sum should be equal to 4", () => {
+  expect(add("2,2")).toBe(4);
 });
